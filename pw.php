82fe76e82289d70a45e8228adff23ebfaa5fc689
Enes Ç <45120208+enescalban@users.noreply.github.com>
2023-05-05 09:54:47 +0300

    Create pw.php

---
+++ pw.php
@@ -0,0 +1,8 @@
+<?php
+function generate_password($length = 12) {
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr(str_shuffle($chars), 0, $length);
+    return $password;
+}
+
+?>
